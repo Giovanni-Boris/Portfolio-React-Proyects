8ffc9e246329f2c75b93cbdb8a967d344c8e47ed
Giovanni-Boris <bancasi@unsa.edu.pe>
2023-02-02 10:54:10 -0500

    Conection database mongodb

--- chat-real-time/node-rest-api/index.js
+++ chat-real-time/node-rest-api/index.js
@@ -1,6 +1,23 @@
-const  express = require("express");
-const  app = express();
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const dotenv = require("dotenv");
 
+
+
+const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.PASSWORD}@cluster0.gyngb.mongodb.net/${process.env.DBNAME}?retryWrites=true&w=majority`;
+
+dotenv.config();
+
+const option = { useNewUrlParser: true, useUnifiedTopology: true}
+
+console.log(process.env.USER_NAME,process.env.DBNAME);
+
+mongoose.connect(uri,option)
+	.then(()=> console.log('Base de datos conectada'))
+	.catch(error => console.log('Error db: ', error))
 
 app.listen(8800,()=>{
 	console.log("Backend server is running! ",8888);

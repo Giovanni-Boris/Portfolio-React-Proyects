b29bdb34fec53a1fe835c0d71d336a26b5318096
Giovanni-Boris <bancasi@unsa.edu.pe>
2023-02-02 16:03:35 -0500

    Creating my model User

--- chat-real-time/node-rest-api/index.js
+++ chat-real-time/node-rest-api/index.js
@@ -3,10 +3,14 @@ const app = express();
 const mongoose = require("mongoose");
 const helmet = require("helmet");
 const morgan = require("morgan");
-const dotenv = require("dotenv");
-mongoose.set('strictQuery', false);
-dotenv.config();
+const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
 
+require("dotenv").config()
+
+mongoose.set('strictQuery', false);
+
+console.log(process.env.URL_MONGO)
 
 const uri = `mongodb+srv://boris-social:qj1DKhAfBDs5gwLI@cluster0.gyngb.mongodb.net/social?retryWrites=true&w=majority
 
@@ -14,9 +18,23 @@ const uri = `mongodb+srv://boris-social:qj1DKhAfBDs5gwLI@cluster0.gyngb.mongodb.
 
 const option = { useNewUrlParser: true, useUnifiedTopology: true}
 
-mongoose.connect(uri,option)
+mongoose.connect(process.env.URL_MONGO,option)
 	.then(()=> console.log('Conected to mongoDB '))
 	.catch(error => console.log('Error db: ', error))
+
+
+//middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
+
+
+
+
 
 app.listen(8800,()=>{
 	console.log("Backend server is running! ",8888);

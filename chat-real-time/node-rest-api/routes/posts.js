b77872c8181326904d725dbf93a3ec61f628540e
Giovanni-Boris <bancasi@unsa.edu.pe>
2023-02-03 09:27:59 -0500

    routes to like a post

--- chat-real-time/node-rest-api/routes/posts.js
+++ chat-real-time/node-rest-api/routes/posts.js
@@ -1,48 +1,59 @@
 const router = require("express").Router();
 
-//create a post 
-router.post("/", async (req,res)=>{
+//create a post
+router.post("/", async (req, res) => {
 	const newPost = new Post(req.body);
-	try{
+	try {
 		const savedPost = await newPost.save();
 		res.status(200).json(savedPost);
-	}catch(err){
+	} catch (err) {
 		res.status(500).json(err);
 	}
-
 });
-//update a post 
-router.put("/:id", async(req,res)=>{
-	try{
+//update a post
+router.put("/:id", async (req, res) => {
+	try {
 		const post = await Post.findById(req.params.id);
-		if(post.userId === req.body.userId){
-			await post.updateOne({ $set:req.body });
+		if (post.userId === req.body.userId) {
+			await post.updateOne({ $set: req.body });
 			res.status(200).json("the post has been updated");
-		}else{
+		} else {
 			res.status(403).json("you can only update your post");
 		}
-	} catch (err){
+	} catch (err) {
 		res.status(500).json(err);
 	}
 });
 //delete ap ost
-router.delete("/:id", async(req,res)=>{
-	try{
+router.delete("/:id", async (req, res) => {
+	try {
 		const post = await Post.findById(req.params.id);
-		if(post.userId === req.body.userId){
+		if (post.userId === req.body.userId) {
 			await post.deleteOne();
 			res.status(200).json("the post has been deleted");
-		}else{
+		} else {
 			res.status(403).json("you can only update your post");
 		}
-	} catch (err){
+	} catch (err) {
 		res.status(500).json(err);
 	}
 });
 //like a post
-
+router.put("/:id/like", async (req, res) => {
+	try {
+		const post = await Post.findById(req.params.id);
+		if (!post.likes.includes(req.body.userId)) {
+			await post.updateOne({ $push: { likes: req.body.userId } });
+			res.status(200).json("The post has been liked");
+		} else {
+			await post.updateOne({ $pull: { likes: req.body.userId } });
+			res.status(200).json("The post has been disliked");
+		}
+	} catch (err) {
+		res.status(500).json(err);
+	}
+});
 //get a post
-
 
 //get a timeline posts
 

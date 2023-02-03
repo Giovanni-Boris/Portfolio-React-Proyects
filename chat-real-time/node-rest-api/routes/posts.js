19700c1d63cb7d3487e4bcab4ade4c1f194b3834
Giovanni-Boris <bancasi@unsa.edu.pe>
2023-02-03 09:12:36 -0500

    Delete post route has been created

--- chat-real-time/node-rest-api/routes/posts.js
+++ chat-real-time/node-rest-api/routes/posts.js
@@ -12,9 +12,33 @@ router.post("/", async (req,res)=>{
 
 });
 //update a post 
-
+router.put("/:id", async(req,res)=>{
+	try{
+		const post = await Post.findById(req.params.id);
+		if(post.userId === req.body.userId){
+			await post.updateOne({ $set:req.body });
+			res.status(200).json("the post has been updated");
+		}else{
+			res.status(403).json("you can only update your post");
+		}
+	} catch (err){
+		res.status(500).json(err);
+	}
+});
 //delete ap ost
-
+router.delete("/:id", async(req,res)=>{
+	try{
+		const post = await Post.findById(req.params.id);
+		if(post.userId === req.body.userId){
+			await post.deleteOne();
+			res.status(200).json("the post has been deleted");
+		}else{
+			res.status(403).json("you can only update your post");
+		}
+	} catch (err){
+		res.status(500).json(err);
+	}
+});
 //like a post
 
 //get a post

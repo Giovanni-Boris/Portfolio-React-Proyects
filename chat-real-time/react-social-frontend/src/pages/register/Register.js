e5e8d79faa06c801741284b89ad6270beb9a29bb
Giovanni-Boris <bancasi@unsa.edu.pe>
2023-02-05 19:32:18 -0500

    Llogin page with axios to fetch and register informatiojn in our api

--- chat-real-time/react-social-frontend/src/pages/register/Register.js
+++ chat-real-time/react-social-frontend/src/pages/register/Register.js
@@ -1,6 +1,39 @@
 import "./register.css";
+import { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
+const initialState = {
+	username: "",
+	email: "",
+	password: "",
+	passwordAgain: "",
+};
 const Register = () => {
+	const [form, setForm] = useState(initialState);
+	const navigate = useNavigate();
+	const handleChange = (e) => {
+		let { name, value } = e.target;
+		setForm({
+			...form,
+			[name]: value,
+		});
+	};
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		if (form.password !== form.passwordAgain) {
+			alert("Passwords don't match!");
+		} else {
+			const { passwordAgain, ...user } = form;
+			try {
+				await axios.post("/auth/register", user);
+				navigate("/login");
+			} catch (err) {
+				console.log(err);
+			}
+		}
+	};
+	console.log(form);
 	return (
 		<div className="login">
 			<div className="loginWrapper">
@@ -11,21 +44,48 @@ const Register = () => {
 						borisocial.
 					</span>
 				</div>
-				<div className="loginRight">
+				<form className="loginRight" onSubmit={handleSubmit}>
 					<div className="loginBox">
-						<input placeholder="Username" className="loginInput" />
-						<input placeholder="Email" className="loginInput" />
-						<input placeholder="Password" className="loginInput" />
+						<input
+							placeholder="Username"
+							required
+							className="loginInput"
+							name="username"
+							onChange={handleChange}
+						/>
+						<input
+							placeholder="Email"
+							required
+							className="loginInput"
+							name="email"
+							onChange={handleChange}
+							type="email"
+						/>
+						<input
+							placeholder="Password"
+							required
+							className="loginInput"
+							name="password"
+							onChange={handleChange}
+							type="password"
+							minLength="6"
+						/>
 						<input
 							placeholder="Password Again"
 							className="loginInput"
+							required
+							name="passwordAgain"
+							onChange={handleChange}
+							type="password"
 						/>
-						<button className="loginButton">Sign Up</button>
+						<button className="loginButton" type="submit">
+							Sign Up
+						</button>
 						<button className="loginRegisterButton">
 							Log into Account
 						</button>
 					</div>
-				</div>
+				</form>
 			</div>
 		</div>
 	);

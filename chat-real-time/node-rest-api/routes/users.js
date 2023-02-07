20bba0ea3af49f5cc5b1ceaf94f76d564642277b
Giovanni-Boris <bancasi@unsa.edu.pe>
2023-02-07 11:03:42 -0500

    resolving some mistakes

--- chat-real-time/node-rest-api/routes/users.js
+++ chat-real-time/node-rest-api/routes/users.js
@@ -76,20 +76,19 @@ router.put("/:id/follow", async (req, res) => {
     try {
       const user = await User.findById(req.params.id);
       const currentUser = await User.findById(req.body.userId);
-      if (!user.followers.include(req.body.userId)) {
+      if (!user.followers.includes(req.body.userId)) {
         await user.updateOne({ $push: { followers: req.body.userId } });
-        await currentUser.updateOne({
-          $push: { followings: req.params.id },
-        });
-        res.status(200).json("user has benn followed");
+        await currentUser.updateOne({ $push: { followings: req.params.id } });
+        res.status(200).json("user has been followed");
       } else {
-        res.status(403).json("you already follow this user");
+        console.log("dsds");
+        res.status(403).json("you allready follow this user");
       }
     } catch (err) {
       res.status(500).json(err);
     }
   } else {
-    res.status(403).json("you can't follow yourself");
+    res.status(403).json("you cant follow yourself");
   }
 });
 //unfollow a user
@@ -98,7 +97,7 @@ router.put("/:id/unfollow", async (req, res) => {
     try {
       const user = await User.findById(req.params.id);
       const currentUser = await User.findById(req.body.userId);
-      if (user.followers.include(req.body.userId)) {
+      if (user.followers.includes(req.body.userId)) {
         await user.updateOne({ $pull: { followers: req.body.userId } });
         await currentUser.updateOne({
           $pull: { followings: req.params.id },
@@ -108,6 +107,7 @@ router.put("/:id/unfollow", async (req, res) => {
         res.status(403).json("you dont follow this user");
       }
     } catch (err) {
+      console.log(err);
       res.status(500).json(err);
     }
   } else {

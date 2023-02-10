ca59213879d89bf47d111d8f5a0df7332ce711f4
Giovanni-Boris <bancasi@unsa.edu.pe>
2023-02-10 11:29:51 -0500

    Get user route created

--- ecommerce/api/routes/user.js
+++ ecommerce/api/routes/user.js
@@ -1,4 +1,4 @@
-const {verifyToken,verifyTokenAndAuthorization} =require("./verifyToken");
+const {verifyToken,verifyTokenAndAuthorization,verifyTokenAndAdmin} =require("./verifyToken");
 const router = require("express").Router();
 const User = require("../models/User");
 
@@ -30,6 +30,17 @@ router.delete("/:id",verifyTokenAndAuthorization,async(req,res)=>{
   try{
     await User.findByIdAndDelete(req.params.id);
     res.status(200).json("User has been deleted");
+  }catch(err){
+    res.status(500).json(err);
+  }
+})
+
+//GET USER
+router.get("/find/:id",verifyTokenAndAdmin,async(req,res)=>{
+  try{
+    const user =  await User.findById(req.params.id);
+    const {password, ...others} = user._doc;
+    res.status(200).json(others);
   }catch(err){
     res.status(500).json(err);
   }

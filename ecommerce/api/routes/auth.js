2ed1aeb1513a573c12ab7919e4aeb7dcef6760fd
Giovanni-Boris <bancasi@unsa.edu.pe>
2023-02-10 10:49:00 -0500

    login proceess suign crypto js and jwt

--- ecommerce/api/routes/auth.js
+++ ecommerce/api/routes/auth.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const User = require("../models/User");
 const CryptoJS = require("crypto-js");
+const jwt = require("jsonwebtoken");
 //REGISTER
 router.post("/register", async (req, res) => {
   console.log("Password secret", process.env.PASS_SEC);
@@ -21,4 +22,40 @@ router.post("/register", async (req, res) => {
   }
 });
 
+//LOGIN
+//LOGIN
+
+router.post("/login", async (req, res) => {
+  try {
+    console.log("JWTTOEKN", process.env.JWT_SEC);
+    const user = await User.findOne({ username: req.body.username });
+    if (!user) return res.status(401).json("Wrong credentials!");
+
+    const hashedPassword = CryptoJS.AES.decrypt(
+      user.password,
+      process.env.PASS_SEC
+    );
+    const OriginalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
+
+    if (OriginalPassword !== req.body.password)
+      return res.status(401).json("Wrong credentials!");
+
+    const accessToken = jwt.sign(
+      {
+        id: user._id,
+        isAdmin: user.isAdmin,
+      },
+      process.env.JWT_SEC,
+      { expiresIn: "3d" }
+    );
+    const { password, ...others } = user._doc;
+
+    res.status(200).json({ ...others, accessToken });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
+

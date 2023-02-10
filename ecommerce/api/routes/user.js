8c518333fd30ed08e81bbf6a2e0838062d729a6f
Giovanni-Boris <bancasi@unsa.edu.pe>
2023-02-10 13:51:36 -0500

    get all users route created

--- ecommerce/api/routes/user.js
+++ ecommerce/api/routes/user.js
@@ -33,7 +33,7 @@ router.delete("/:id",verifyTokenAndAuthorization,async(req,res)=>{
   }catch(err){
     res.status(500).json(err);
   }
-})
+});
 
 //GET USER
 router.get("/find/:id",verifyTokenAndAdmin,async(req,res)=>{
@@ -41,6 +41,17 @@ router.get("/find/:id",verifyTokenAndAdmin,async(req,res)=>{
     const user =  await User.findById(req.params.id);
     const {password, ...others} = user._doc;
     res.status(200).json(others);
+  }catch(err){
+    res.status(500).json(err);
+  }
+});
+
+//GET ALL USER
+router.get("/",verifyTokenAndAdmin,async(req,res)=>{
+  const query = req.query.new;
+  try{
+    const users = query ? await User.find().sort({_id:-1}).limit(5) : await User.find();
+    res.status(200).json(users);
   }catch(err){
     res.status(500).json(err);
   }

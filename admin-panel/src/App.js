48a6b7c6bcfd86ff1440427b3dcf81e853d43eea
Giovanni-Boris <bancasi@unsa.edu.pe>
2023-02-13 14:15:10 -0500

    sidebar component created

--- admin-panel/src/App.js
+++ admin-panel/src/App.js
@@ -1,8 +1,14 @@
 import Navbar from "./components/Navbar/Navbar";
+import Sidebar from "./components/Sidebar/Sidebar";
+import "./app.css";
 function App() {
   return (
     <div>
       <Navbar />
+      <div className="container">
+        <Sidebar />
+        <div className="others">Other</div>
+      </div>
     </div>
   );
 }

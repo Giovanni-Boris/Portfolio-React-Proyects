7a59f6232eedea18e85ab3c05241b38271ec8263
Giovanni-Boris <bancasi@unsa.edu.pe>
2023-02-13 09:41:01 -0500

    creating another slice to make userRedux

--- ecommerce/client/src/redux/store.js
+++ ecommerce/client/src/redux/store.js
@@ -1,9 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from "./cartRedux";
-
+import useReducer from "./userRedux";
 console.log(cartReducer);
 export default configureStore({
   reducer: {
     cart: cartReducer,
+    user: useReducer,
   },
 });

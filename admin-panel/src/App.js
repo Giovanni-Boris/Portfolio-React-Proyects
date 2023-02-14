73397c287a0ff282b532da070299e72ea55e76d6
Giovanni-Boris <bancasi@unsa.edu.pe>
2023-02-14 13:51:19 -0500

    Userlist page created

--- admin-panel/src/App.js
+++ admin-panel/src/App.js
@@ -1,16 +1,21 @@
 import Navbar from "./components/Navbar/Navbar";
 import Sidebar from "./components/Sidebar/Sidebar";
 import Home from "./pages/Home/Home";
+import UserList from "./pages/userList/UserList";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./app.css";
 function App() {
   return (
-    <div>
+    <Router>
       <Navbar />
       <div className="container">
         <Sidebar />
-        <Home />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/users" element={<UserList />} />
+        </Routes>
       </div>
-    </div>
+    </Router>
   );
 }
 

7a72618a53db2c5f75c976fbacac49622215be24
Giovanni-Boris <bancasi@unsa.edu.pe>
2023-02-14 14:44:03 -0500

    User page created

--- admin-panel/src/App.js
+++ admin-panel/src/App.js
@@ -1,7 +1,8 @@
 import Navbar from "./components/Navbar/Navbar";
 import Sidebar from "./components/Sidebar/Sidebar";
-import Home from "./pages/Home/Home";
+import Home from "./pages/home/Home";
 import UserList from "./pages/userList/UserList";
+import User from "./pages/user/User";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./app.css";
 function App() {
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/users" element={<UserList />} />
+          <Route path="/user/:userId" element={<User />} />
         </Routes>
       </div>
     </Router>

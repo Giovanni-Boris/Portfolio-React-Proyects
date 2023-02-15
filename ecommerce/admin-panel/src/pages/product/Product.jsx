125487979733e89aa86e106ee7059348c88f0bba
Giovanni-Boris <bancasi@unsa.edu.pe>
2023-02-15 10:59:48 -0500

    coping my admin-panel -to my ecommerce

---
+++ ecommerce/admin-panel/src/pages/product/Product.jsx
@@ -0,0 +1,80 @@
+import "./product.css";
+import { Link } from "react-router-dom";
+import Chart from "../../components/chart/Chart";
+import { productData } from "../../dummyData";
+import PublishIcon from "@mui/icons-material/Publish";
+
+const Product = () => {
+  return (
+    <div className="product">
+      <div className="productTitleContainer">
+        <h1 className="productTitle">Product</h1>
+        <Link to="/newProduct">
+          <button className="productAddButton">Create</button>
+        </Link>
+      </div>
+      <div className="productTop">
+        <div className="productTopLeft">
+          <Chart data={productData} dataKey="Sales" title="Sales Performance" />
+        </div>
+        <div className="productTopRight">
+          <div className="productInfoTop">
+            <img
+              src="https://images.pexels.com/photos/1152994/pexels-photo-1152994.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+              alt=""
+              className="productInfoImg"
+            />
+            <span className="productName">{"Apple"}</span>
+          </div>
+          <div className="productInfoBottom">
+            <div className="productInfoItem">
+              <span className="productInfoKey">id:</span>
+              <span className="productInfoValue">{"3131313"}</span>
+            </div>
+            <div className="productInfoItem">
+              <span className="productInfoKey">sales:</span>
+              <span className="productInfoValue">$123</span>
+            </div>
+            <div className="productInfoItem">
+              <span className="productInfoKey">in stock</span>
+              <span className="productInfoValue">{"True"}</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="productBottom">
+        <form className="productForm">
+          <div className="productFormLeft">
+            <label>Product Name</label>
+            <input type="text" placeholder="apple" />
+            <label>Product Description</label>
+            <input type="text" placeholder="airpots" />
+            <label>Price</label>
+            <input type="text" placeholder="$ 4.00" />
+            <label>In Stock</label>
+            <select name="inStock" id="idStock">
+              <option value="true">Yes</option>
+              <option value="false">No</option>
+            </select>
+          </div>
+          <div className="productFormRight">
+            <div className="productUpload">
+              <img
+                src="https://images.pexels.com/photos/1152994/pexels-photo-1152994.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+                alt=""
+                className="productUploadImg"
+              />
+              <label htmlFor="file">
+                <PublishIcon />
+              </label>
+              <input type="file" id="file" style={{ display: "none" }} />
+            </div>
+            <button className="productButton">Update</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Product;
---
+++ ecommerce/admin-panel/src/pages/productList/ProductList.jsx
@@ -0,0 +1,66 @@
+import "./productList.css";
+import { DataGrid } from "@mui/x-data-grid";
+import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
+import { Link } from "react-router-dom";
+import { productRows } from "../../dummyData";
+import { useState } from "react";
+const ProductList = () => {
+  const [data, setData] = useState(productRows);
+  console.log(data);
+  const handleDelete = (id) => {
+    setData(data.filter((item) => item.id !== id));
+  };
+  const columns = [
+    { field: "id", headerName: "ID", width: 220 },
+    {
+      field: "product",
+      headerName: "Product",
+      width: 220,
+      renderCell: (params) => {
+        return (
+          <div className="productListItem">
+            <img className="productListImg" src={params.row.img} alt="" />
+            {params.row.name}
+          </div>
+        );
+      },
+    },
+    { field: "stock", headerName: "Stock", width: 200 },
+    {
+      field: "price",
+      headerName: "Price",
+      width: 160,
+    },
+    {
+      field: "action",
+      headerName: "Action",
+      width: 150,
+      renderCell: (params) => {
+        return (
+          <>
+            <Link to={"/product/" + params.row.id}>
+              <button className="productListEdit">Edit</button>
+            </Link>
+            <DeleteOutlineIcon
+              className="productListDelete"
+              onClick={() => handleDelete(params.row.id)}
+            />
+          </>
+        );
+      },
+    },
+  ];
+  return (
+    <div className="productList">
+      <DataGrid
+        rows={data}
+        disableSelectionOnClick
+        columns={columns}
+        pageSize={8}
+        checkboxSelection
+      />
+    </div>
+  );
+};
+
+export default ProductList;

7c87f53d70103ad8ba74254835f225d6849750bf
Giovanni-Boris <bancasi@unsa.edu.pe>
2023-02-15 10:27:13 -0500

    Product page created

--- admin-panel/src/App.js
+++ admin-panel/src/App.js
@@ -5,6 +5,7 @@ import UserList from "./pages/userList/UserList";
 import User from "./pages/user/User";
 import NewUser from "./pages/newUser/NewUser";
 import ProductList from "./pages/productList/ProductList";
+import Product from "./pages/product/Product";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./app.css";
 function App() {
@@ -19,7 +20,7 @@ function App() {
           <Route path="/user/:userId" element={<User />} />
           <Route path="/newUser" element={<NewUser />} />
           <Route path="/products" element={<ProductList />} />
-          <Route path="/product/:productId" element={<User />} />
+          <Route path="/product/:productId" element={<Product />} />
           <Route path="/newProduct" element={<NewUser />} />
         </Routes>
       </div>

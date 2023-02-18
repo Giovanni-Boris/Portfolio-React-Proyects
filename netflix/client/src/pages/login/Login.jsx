dd83a5808389aaac3a22005c1cc6122af27fc795
Giovanni-Boris <bancasi@unsa.edu.pe>
2023-02-18 16:17:43 -0500

    Login page created

---
+++ netflix/client/src/pages/login/Login.jsx
@@ -0,0 +1,64 @@
+import "./login.scss";
+import { useState } from "react";
+
+const initialState = {
+  password: "",
+  username: "",
+};
+
+const Login = () => {
+  const [form, setForm] = useState(initialState);
+
+  const handleChange = (e) => {
+    let { name, value } = e.target;
+    setForm({
+      ...form,
+      [name]: value,
+    });
+  };
+
+  const handleLogin = () => {};
+  const handleFinish = () => {};
+  return (
+    <div className="login">
+      <div className="top">
+        <div className="wrapper">
+          <img
+            className="logo"
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2560px-Netflix_2015_logo.svg.png"
+            alt=""
+          />
+        </div>
+      </div>
+      <div className="container">
+        <form>
+          <h1>Sign In</h1>
+          <input
+            type="email"
+            name="email"
+            placeholder="Email or phone number"
+            onChange={handleChange}
+          />
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            onChange={handleChange}
+          />
+          <button className="loginButton" onClick={handleLogin}>
+            Sign In
+          </button>
+          <span>
+            New to Netflix? <b>Sign up now.</b>
+          </span>
+          <small>
+            This page is protected by Google reCAPTCHA to ensure you're not a
+            bot. <b>Learn more</b>.
+          </small>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;

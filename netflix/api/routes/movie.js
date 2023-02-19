c120cbd091d7a405629fed8f2ab13f7db9cd169a
Giovanni-Boris <bancasi@unsa.edu.pe>
2023-02-19 12:37:53 -0500

    Deleted movie endpoint created

--- netflix/api/routes/movie.js
+++ netflix/api/routes/movie.js
@@ -5,12 +5,29 @@ const {
   verifyTokenAndAdmin,
 } = require("./verifyToken");
 
+//created
 route.post("/", verifyTokenAndAdmin, async (req, res) => {
   const newMovie = new Movie(req.body);
 
   try {
     const savedMovie = await newMovie.save();
     res.status(201).json(savedMovie);
+  } catch (err) {
+    res.status(500).json(500);
+  }
+});
+
+//UPDATE
+route.put("/:id", verifyTokenAndAdmin, async (req, res) => {
+  try {
+    const updateMovie = await Movie.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    res.status(201).json(updateMovie);
   } catch (err) {
     res.status(500).json(500);
   }

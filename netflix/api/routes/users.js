dbdc1c5002493b6a1ef6b7f53356b89bc1197ea1
Giovanni-Boris <bancasi@unsa.edu.pe>
2023-02-19 12:04:31 -0500

    Update user endpoint created

---
+++ netflix/api/routes/users.js
@@ -0,0 +1,38 @@
+const router = require("express").Router();
+const User = require("../models/User");
+const CryptoJS = require("crypto-js");
+const { verifyTokenAndAuthorization } = require("./verifyToken");
+
+//UPDATE
+router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
+  if (req.body.password) {
+    req.body.password = CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.SECRET_KEY
+    ).toString();
+  }
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      {
+        new: true,
+      }
+    );
+    res.status(200).json(updatedUser);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//DELETE
+
+//GET
+
+//GET ALL
+
+//GET USER START
+
+module.exports = router;

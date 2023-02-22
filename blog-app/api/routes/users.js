d7687c0b2150e864c63ed8c82b3b77ba01f92e2e
Giovanni-Boris <bancasi@unsa.edu.pe>
2023-02-22 10:42:05 -0500

    Delete user route created

--- blog-app/api/routes/users.js
+++ blog-app/api/routes/users.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const User = require("../models/User");
+const Post = require("../models/Post");
 const bcrypt = require("bcrypt");
 const { verifyTokenAndAuthorization } = require("./verifyToken");
 //REGISTER
@@ -19,5 +20,21 @@ router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
     res.status(201).json(updatedUser);
   } catch (err) {
     res.status(500).json(err);
+  }
+});
+
+//DELETE
+router.delete("/:id", verifyTokenAndAuthorization, async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    try {
+      await Post.deleteMany({ username: user.username });
+      await User.findByIdAndDelete(req.params.id);
+      res.status(200).json("User has been deleted...");
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  } catch (err) {
+    res.status(404).json("User not found");
   }
 });

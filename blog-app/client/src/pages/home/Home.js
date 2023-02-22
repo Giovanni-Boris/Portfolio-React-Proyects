7b9f64dd3f7d0ff82b9f101fa2c715abf2bc3679
Giovanni-Boris <bancasi@unsa.edu.pe>
2023-02-22 14:14:37 -0500

    heder component created

---
+++ blog-app/client/src/pages/home/Home.js
@@ -0,0 +1,13 @@
+import "./home.css";
+import Header from "../../components/header/Header";
+
+const Home = () => {
+  return (
+    <div className="home">
+      <Header />
+      Homepage
+    </div>
+  );
+};
+
+export default Home;

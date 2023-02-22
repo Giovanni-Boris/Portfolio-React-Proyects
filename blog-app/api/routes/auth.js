01b482731a599f5a6f43b17e9b91c04334773bfc
Giovanni-Boris <bancasi@unsa.edu.pe>
2023-02-22 10:24:22 -0500

    Register roue endpoint created

--- blog-app/api/routes/auth.js
+++ blog-app/api/routes/auth.js
@@ -1,4 +1,4 @@
-const router = require("express").Route();
+const router = require("express").Router();
 const User = require("../models/User");
 const bcrypt = require("bcrypt");
 
@@ -13,7 +13,7 @@ router.post("/register", async (req, res) => {
       password: hashedPass,
     });
     const user = await newUser.save();
-    res.status(200).json(user);
+    res.status(201).json(user);
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
@@ -21,5 +21,20 @@ router.post("/register", async (req, res) => {
 });
 
 //LOGIN
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    if (!user) return res.status(400).json("Wrong credentials!");
+
+    const validated = await bcrypt.compare(req.body.password, user.password);
+    if (!validated) return res.status(400).json("Wrong credentials!");
+
+    const { password, ...others } = user._doc;
+
+    res.status(200).json(others);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;

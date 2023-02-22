efae6838e2ec42afb0d8b975ff88fda4117a08cb
Giovanni-Boris <bancasi@unsa.edu.pe>
2023-02-22 11:19:03 -0500

    Get all categories

--- blog-app/api/routes/categories.js
+++ blog-app/api/routes/categories.js
@@ -12,4 +12,13 @@ router.post("/", verifyToken, async (req, res) => {
   }
 });
 
+router.get("/", verifyToken, async (req, res) => {
+  try {
+    const cats = await Category.find();
+    res.status(200).json(cats);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;

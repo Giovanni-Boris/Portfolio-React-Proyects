477c051e6cc0f42c775f8172cc37e9af09a10f04
Giovanni-Boris <bancasi@unsa.edu.pe>
2023-02-22 09:55:46 -0500

    Creating my initial configurations

---
+++ blog-app/api/index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const morgan = require("morgan");
+const app = express();
+
+//config
+dotenv.config();
+mongoose.set("strictQuery", false);
+
+//console.log(process.env.MONGO_URL);
+const option = { useNewUrlParser: true, useUnifiedTopology: true };
+mongoose
+  .connect(process.env.MONGO_URL, option)
+  .then(() => console.log("DB connection successfull"))
+  .catch((error) => console.log("Error db: ", error));
+
+//midleware
+// cors
+const corsOption = {
+  origin: "#",
+  optionsSuccessStatus: 200,
+};
+//other host can be reach to my api
+app.use(cors({ corsOption }));
+// is like body-parse but express.json is native from express
+app.use(express.json());
+//to draw in my console the request and the response eithter it wsa successfull or error
+app.use(morgan("common"));
+
+app.listen(5000, () => {
+  console.log("Backend running in PORT 5000");
+});

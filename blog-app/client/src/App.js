8e7aac8ea05abb0ff444112817515c35b12b67d0
Giovanni-Boris <bancasi@unsa.edu.pe>
2023-02-23 10:22:40 -0500

    login page created

--- blog-app/client/src/App.js
+++ blog-app/client/src/App.js
@@ -3,11 +3,12 @@ import Home from "./pages/home/Home";
 import Write from "./pages/write/Write";
 import Single from "./pages/single/Single";
 import Settings from "./pages/settings/Settings";
+import Login from "./pages/login/Login";
 function App() {
   return (
     <>
       <Topbar />
-      <Settings />
+      <Login />
     </>
   );
 }

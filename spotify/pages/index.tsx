bb1e0095d3d99f2eafbfbc2b9578473b3a860c13
Giovanni-Boris <bancasi@unsa.edu.pe>
2023-03-02 20:41:18 -0500

    My setup odf next

---
+++ spotify/pages/index.tsx
@@ -0,0 +1,18 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+
+const Home: NextPage = () => {
+  return (
+    <div className="">
+      <h1>My first next</h1>
+
+      <main>
+        {/* Sidebar*/}
+        {/* main content */}
+      </main>
+    </div>
+  );
+};
+
+export default Home;

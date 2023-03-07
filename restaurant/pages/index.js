95e66139e6ae56284ac7232929f0a88f677032ac
Giovanni-Boris <bancasi@unsa.edu.pe>
2023-03-07 11:20:13 -0500

    Feature d componenet created

--- restaurant/pages/index.js
+++ restaurant/pages/index.js
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import styles from "@/styles/Home.module.css";
-
+import Featured from "../components/Featured";
 export default function Home() {
   return (
     <>
@@ -10,7 +10,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      Hoemapge
+      <Featured />
     </>
   );
 }

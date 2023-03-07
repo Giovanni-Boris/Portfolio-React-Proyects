e44d697ec78d086c89243ecb9f0f7d97715f2e5f
Giovanni-Boris <bancasi@unsa.edu.pe>
2023-03-07 13:47:55 -0500

    Pizza cart has been done

--- restaurant/components/PizzaCard.js
+++ restaurant/components/PizzaCard.js
@@ -4,14 +4,12 @@ import Image from "next/image";
 const PizzaCard = () => {
   return (
     <div className={styles.container}>
-      <Image src="/img/pizza.png" alt="" width="500" height="200"/>
+      <Image src="/img/pizza.png" alt="" width="200" height="200" />
       <h1 className={styles.title}>title</h1>
-          <span className={styles.price}>$19 </span>
-        <p className={styles.desc}>
-          Lorem   
-        </p>
+      <span className={styles.price}>$19 </span>
+      <p className={styles.desc}>Lorem</p>
     </div>
-  ) 
-}
+  );
+};
 
-export default PizzaCard
+export default PizzaCard;

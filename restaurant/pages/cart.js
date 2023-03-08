be80a74658485e7b6663acff00e05b31cd2e4b98
Giovanni-Boris <bancasi@unsa.edu.pe>
2023-03-07 20:35:26 -0500

    cart page created

---
+++ restaurant/pages/cart.js
@@ -0,0 +1,68 @@
+import styles from "../styles/Cart.module.css";
+import Image from "next/image";
+const Cart = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.left}>
+        <table className={styles.table}>
+          <thead className={styles.thead}>
+            <tr className={styles.trTitle}>
+              <th>Product</th>
+              <th>Name</th>
+              <th>Extras</th>
+              <th>Price</th>
+              <th>Quantity</th>
+              <th>Total</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr className={styles.tr}>
+              <td>
+                <div className={styles.imgContainer}>
+                  <Image src="/img/pizza.png" fill alt="" />
+                </div>
+              </td>
+              <td>
+                <span className={styles.name}>CORALZO</span>
+              </td>
+              <td>
+                <span className={styles.extras}>
+                  <span>dOUBLE inGREIANTS</span>
+                </span>
+              </td>
+              <td>
+                <span className={styles.price}>$19</span>
+              </td>
+              <td>
+                <span className={styles.quantity}>2</span>
+              </td>
+              <td>
+                <span className={styles.total}>$80</span>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <div className={styles.right}>
+        <div className={styles.wrapper}>
+          <h2 className={styles.title}>CART TOTAL</h2>
+          <div className={styles.totalText}>
+            <b className={styles.totalTextTitle}>Subtotal:</b>$90
+          </div>
+          <div className={styles.totalText}>
+            <b className={styles.totalTextTitle}>Discount:</b>$0.00
+          </div>
+          <div className={styles.totalText}>
+            <b className={styles.totalTextTitle}>Total:</b>$20
+          </div>
+          <div className={styles.paymentMethods}>
+            <button className={styles.payButton}>CASH ON DELIVERY</button>
+          </div>
+          <button className={styles.button}>CHECKOUT NOW!</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Cart;

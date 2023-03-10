90376607d0dab437c9c47eb37a89e5677248fa84
Giovanni-Boris <bancasi@unsa.edu.pe>
2023-03-09 19:30:54 -0500

    next state in progress

--- restaurant/pages/admin/index.js
+++ restaurant/pages/admin/index.js
@@ -23,9 +23,14 @@ const Index = ({ orders, products }) => {
     const item = orderList.filter((order) => order._id === id)[0];
     const currentStatus = item.status;
     try {
-      const res = await axios.put("http://localhost:3000/orders/" + id, {
+      const res = await axios.put("http://localhost:3000/api/orders/" + id, {
         status: currentStatus + 1,
       });
+      console.log(orderList, res.data);
+      setOrderList([
+        res.data,
+        ...orderList.filter((order) => order._id !== id),
+      ]);
     } catch (err) {
       console.log(res);
     }

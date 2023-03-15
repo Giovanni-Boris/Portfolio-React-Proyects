5e4f59ea51159a03caaf522c2cccbfe846616eb7
Giovanni-Boris <bancasi@unsa.edu.pe>
2023-03-15 11:11:27 -0500

    giving some instructions about url

--- restaurant/lib/requestMethod.js
+++ restaurant/lib/requestMethod.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
+//Use this url if you want to run this url on your computer
+
 //const BASE_URL = "http://localhost:3000/api/";
 
+//Use this Url if you want to deploy on vercel
 const BASE_URL = "https://portfolio-react-proyects.vercel.app/api/";
 export const publicRequest = axios.create({
   baseURL: BASE_URL,

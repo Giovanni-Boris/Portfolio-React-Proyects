38dc6e631015e9a3299feb5b68c7db7afbea9c26
Giovanni-Boris <bancasi@unsa.edu.pe>
2023-03-16 12:18:07 -0500

    Using hearders intead header

--- ecommerce/admin-panel/src/requestMethod.js
+++ ecommerce/admin-panel/src/requestMethod.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-//const BASE_URL = "http://localhost:8800/api/";
+const BASE_URL = "http://localhost:8800/api/";
 
-const BASE_URL = "https://ecommerce-ffd8.onrender.com/api/";
+//const BASE_URL = "https://ecommerce-ffd8.onrender.com/api/";
 const TOKEN =
   localStorage.getItem("persist:root") &&
   JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user)?.currentUser
@@ -14,5 +14,5 @@ export const publicRequest = axios.create({
 
 export const userRequest = axios.create({
   baseURL: BASE_URL,
-  header: { token: `Bearer ${TOKEN}` },
+  headers: { token: `Bearer ${TOKEN}` },
 });

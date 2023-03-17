2d2b8afe8d75982c5d90503f66921e97c7335648
Giovanni-Boris <bancasi@unsa.edu.pe>
2023-03-16 21:49:35 -0500

    list updated

--- netflix/client/src/components/list/List.jsx
+++ netflix/client/src/components/list/List.jsx
@@ -6,7 +6,7 @@ import { useRef, useState } from "react";
 const List = ({ list }) => {
   const [slideNumber, setSlideNumber] = useState(0);
   const [isMoved, setIsMoved] = useState(false);
-  const [clickLimit, setClickLimit] = useState(window.innerWidth / 230);
+  const clickLimit = window.innerWidth / 230;
   const listRef = useRef();
 
   const handleClick = (direction) => {

8aad57bd4f116f7560830e8bc3ddf29bae575dd9
bancasi <bancasi@unsa.edu.pe>
2023-03-18 16:45:25 -0300

    Footer section finished

---
+++ portfolio/src/components/footer/Footer.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import "./footer.css";
+import { FaHome, FaPhone, FaMailBulk, FaFacebook, FaLinkedin } from "react-icons/fa";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="footer-container">
+        <div className="left">
+          <div className="location">
+            <FaHome size={20} style={{ color:"#fff", marginRight:"2rem"}}/>
+            <div>
+              <p>123 Hacker Rank</p>
+            </div>
+          </div>
+          <div className="phone">
+            <h4>
+              <FaPhone size={20} style={{ color:"#fff", marginRight:"2rem"}}/>
+              +51 954 - 464 - 645
+            </h4>
+          </div>
+          <div className="email">
+            <h4>
+              <FaMailBulk size={20} style={{ color:"#fff", marginRight:"2rem"}}/>
+              bancasi@unsa.edu.pe
+            </h4> 
+          </div>
+        </div>
+        <div className="right">
+          <FaFacebook size={40} style={{ color:"#fff", marginRight:"2rem"}}/>
+          <FaLinkedin size={40} style={{ color:"#fff", marginRight:"2rem"}}/>
+        </div>
+      </div>
+    </div>
+  )
+} 
+
+export default Footer
